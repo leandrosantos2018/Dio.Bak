9f8f3d62c639bcf1b0a6a51d4619063926760d07
Leandro Santos <leleuramos@gmail.com>
2021-09-15 21:21:55 -0300

    foi implementado os metados e criação do menu

---
+++ Enum/TipoConta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dio.Bak
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuritica= 2
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,99 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 namespace Dio.Bak
 {
     class Program
     {
+        static List<Conta> listaContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta();
-            minhaConta.Nome = 'Eliezer'
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-            Console.WriteLine("Hello World!");
+            while(opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario) 
+                {
+                    case "1":
+                        ListaConta();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                       // Transferir();
+                        break;
+                    case "4":
+                        //Sacar();
+                        break;
+                    case "5":
+                       // Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos Serviços");
+            Console.ReadLine();
         }
+
+        private static void ListaConta()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir Nova Conta");
+
+            Console.WriteLine("Digite 1 para conta Fisica ou 2 para Juridica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do CLiente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.WriteLine("Digite o Saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Credito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                        saldo: entradaSaldo,
+                                        credito: entradaCredito,
+                                        nome: entradaNome);
+
+            listaContas.Add(novaConta);
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Dio BANk a Seu dispor!!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1- Lista Contas");
+            Console.WriteLine("2- Inserir nova Conta");
+            Console.WriteLine("3- Transferir");
+            Console.WriteLine("4- Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar a Tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+
     }
 }
